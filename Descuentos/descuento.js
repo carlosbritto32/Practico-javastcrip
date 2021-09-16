7d54ef8e417afab4ba69111201ab6b81913940b5
Carlos Britto <carlosbritto32@gmail.com>
2021-09-16 11:22:53 -0500

    Se crean funciones para dar usabilidad a la pagina en el archivo js

--- Descuentos/descuento.js
+++ Descuentos/descuento.js
@@ -1,1 +1,32 @@
+// const precioOriginal = 120;
+// const descuento = 18;
 
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+  return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("inputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("inputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultPrice = document.getElementById("resultPrice");
+  resultPrice.innerText = `El precio con descuento es: $${precioConDescuento} `;
+}
+
+// console.log({
+//   precioOriginal,
+//   descuento,
+//   porcentajePrecioConDescuento,
+//   precioConDescuento,
+// });
